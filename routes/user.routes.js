aa0886aed61a4b753af8c0687d63a42b0ffa4ed0
vinisi12363 <viniciuspv.si@gmail.com>
2023-05-31 10:25:55 -0300

    feat: adding the schema folder and login validation

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { signUpValidation } from "../middlewares/userValidation.middelware.js";
-import { signUp } from "../controllers/user.controllers.js";
+import {signInValidation} from "../middlewares/userValidation.middelware.js";
+import { signUp, singIn } from "../controllers/user.controllers.js";
+
+import { loginSchema } from "../schemas/user_login.schema.js";
 
 const userRouter = Router()
 
 userRouter.post("/sign-up", signUpValidation, signUp)
+userRouter.post("/sign-in",signInValidation(loginSchema), singIn )
 
 export default userRouter
---
+++ schemas/user_login.schema.js
@@ -0,0 +1,6 @@
+import Joi from 'joi'
+
+ export const loginSchema = Joi.object({
+   email: Joi.string().email().required(),
+   password: Joi.string().min(6).required(),
+ });
