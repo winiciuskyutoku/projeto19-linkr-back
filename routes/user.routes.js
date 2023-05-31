84a4b5b393e3b443ea8b3c6619e2d3c63a361900
vinisi12363 <viniciuspv.si@gmail.com>
2023-05-31 10:45:14 -0300

    adding fixes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { signUpValidation } from "../middlewares/userValidation.middelware.js";
 import {signInValidation} from "../middlewares/loginValidation.middleware.js";
 import { signUp, singIn } from "../controllers/user.controllers.js";
-
 import { loginSchema } from "../schemas/user_login.schema.js";
 
 const userRouter = Router()
