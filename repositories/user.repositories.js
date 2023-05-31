44f17540e6f9c364ec40643091414376a250942b
vinisi12363 <viniciuspv.si@gmail.com>
2023-05-31 14:00:29 -0300

    fixing login

--- repositories/user.repositories.js
+++ repositories/user.repositories.js
@@ -7,30 +7,30 @@ export async function signUpRepository(body){
     const hash = bcrypt.hashSync(password, 10)
     return await db.query(`INSERT INTO users (username, user_password, user_photo, user_email) VALUES ($1, $2, $3, $4);`, [name, hash, image, email])
 }
-export async function singInRepository(body) {
-  const { email, password } = body;
-  try {
-    const result = await db.query(
-      `SELECT user_id, username, user_photo, user_password  
-       FROM users 
-       WHERE user_email=$1`,
-      [email]
-    );
+    export async function singInRepository(body) {
+    const { email, password } = body;
+    try {
+        const result = await db.query(
+        `SELECT user_id, username, user_photo, user_password  
+        FROM users 
+        WHERE user_email=$1`,
+        [email]
+        );
 
-    if (result.rowCount > 0) {
-      const passwordMatch = bcrypt.compareSync(password, result.rows[0].user_password);
-      
-      
-      if (passwordMatch) {
-        return result
-      }
+        if (result.rowCount > 0) {
+        const passwordMatch = bcrypt.compareSync(password, result.rows[0].user_password);
+        
+        
+        if (passwordMatch) {
+            return result
+        }
 
+        }
+
+
+        return null; 
+    } catch (err) {
+        console.log("erro na query", err.message);
+        throw err;
     }
-
-
-    return null; // Retorna null caso não haja correspondência de email ou senha
-  } catch (err) {
-    console.log("erro na query", err.message);
-    throw err;
-  }
-}
+    }
