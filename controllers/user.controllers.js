bd77720533b3b1fb3bd9f97c542555926c8757fe
vinisi12363 <viniciuspv.si@gmail.com>
2023-06-01 20:37:50 -0300

    adding fixes

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,7 +1,5 @@
-
- import { signUpRepository, singInRepository, getUsersDB, getUserByIdDB } from "../repositories/user.repositories.js"
-
-import authService from "../services/authService.js";
+ import { signUpRepository, singInRepository } from "../repositories/user.repositories.js"
+ import authService from "../services/authService.js";
 
 export async function signUp(req, res){
     try {
@@ -9,6 +7,7 @@ export async function signUp(req, res){
 
         res.status(201).send({ message: "Conta criado com sucesso." })
     } catch (err) {
+
         res.status(500).send(err.mesasge)
     }
 }
@@ -16,11 +15,25 @@ export async function signUp(req, res){
 
 export async function singIn(req, res) {
     try {
-        const result = await singInRepository(req.body)
+        const result = azwait singInRepository(req.body)
 
         if (!result || result === null) {
             return res.status(400).send("email ou senha estão incorretos")
         }
+<<<<<<< HEAD
+
+
+        res.status(200).send({
+            user_id: result.rows[0].user_id,
+            username: result.rows[0].username,
+            user_photo: result.rows[0].user_photo,
+        });
+
+
+    } catch (err) {
+
+        res.status(500).send(err.message)
+=======
         
           const token = authService.generateWebToken(result.rows[0].user_id)
           res.status(200).send({
@@ -34,6 +47,7 @@ export async function singIn(req, res) {
     }catch(err){
          console.log(err.message)
          res.status(500).send(err.mesasge)
+>>>>>>> 017c2fc (feat: login token)
     }
 
 }
@@ -48,12 +62,3 @@ export async function getUsers(req, res) {
     }
 }
 
-export async function getProfile(req, res) {
-    const { id } = req.params;
-    try{
-        const userProfile = await getUserByIdDB(id);
-        res.send(userProfile.rows);
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-}
