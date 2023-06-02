3cefd056bd517be95e7e3e7d30206628b73b9b09
vinisi12363 <viniciuspv.si@gmail.com>
2023-06-01 21:03:23 -0300

    tentando arrumar o bug do getprofiles

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { signUpValidation } from "../middlewares/userValidation.middelware.js";
 import schemaValidation from "../middlewares/schemaValidation.middleware.js";
-import { signUp, singIn,  } from "../controllers/user.controllers.js";
+import {
+  signUp,
+  singIn,
+  getUsers,
+} from "../controllers/user.controllers.js";
 import { loginSchema, signUpValidate, searchValidate } from "../schemas/userValidate.schema.js";
 
 
@@ -9,7 +13,7 @@ const userRouter = Router();
 
 userRouter.post("/sign-up", schemaValidation(signUpValidate), signUpValidation, signUp);
 userRouter.post("/sign-in", schemaValidation(loginSchema), singIn);
-//userRouter.post("/get-users", schemaValidation(searchValidate), getUsers);
+userRouter.post("/get-users", schemaValidation(searchValidate), getUsers);
 //userRouter.get("/profile-user/:id", getProfile)
 
 
