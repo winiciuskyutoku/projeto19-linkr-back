0e3aacc8f837ad7bc5c8ae99207aab43be0ad9e2
vinisi12363 <viniciuspv.si@gmail.com>
2023-06-01 21:07:34 -0300

    tentando arrumar o bug do getprofiles

--- database/database.connection.js
+++ database/database.connection.js
@@ -6,7 +6,7 @@ dotenv.config()
 const { Pool } = pg
 
 const configDatabase = {
-    connectionString: "postgresql://linkrdbuser:senha_super_secreta_do_dbuser@localhost:5432/linkrdatabase"
+    connectionString: process.env.DATABASE_URL
 }
 
 if (process.env.MODE === "prod") configDatabase.ssl = true;
