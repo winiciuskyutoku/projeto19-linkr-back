c108588d4270b3d2f07ad5572806fa9fbb43f388
vinisi12363 <viniciuspv.si@gmail.com>
2023-06-06 20:59:48 -0300

    resolving Fixes

--- middlewares/verifyEmail.middleware.js
+++ middlewares/verifyEmail.middleware.js
@@ -1,6 +1,20 @@
 import { db } from "../database/database.connection.js";
 
-
+export async function signUpValidation(req, res, next) {
+  try {
+    const result = await db.query(
+      `SELECT user_email FROM users $1 `,
+      [req.body.email]
+    );
+    if (result.rowCount > 0)
+      return res
+        .status(409)
+        .send({ message: "Esse email já cadastrado!" });
+    next();
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
 
 export async function getUserByEmail(email) {
   try {
