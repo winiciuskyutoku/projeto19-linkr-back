931002838e3e5fd69f7d687a54a1f3a46076ff27
Savio Dias Viana <saviodiasviana@gmail.com>
2023-06-09 18:17:23 -0300

    Rota da timeline do usuário logado

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -5,14 +5,13 @@ import { postSchema } from "../schemas/post.schemas.js";
 import { getPosts, postPosts, likePost, deletePost, getNewPostsAmount, getPostsLogin } from "../controllers/post.controllers.js";
 import schemaValidation from "../middlewares/schemaValidation.middleware.js";
 import { validateDeletePost } from "../middlewares/post.middleware.js";
-import { timestampSchema } from "../schemas/timestamp.schema.js";
 
 const postsRouter = Router()
 
 postsRouter.get("/get-new-posts/:date", authValidation, getNewPostsAmount)
 postsRouter.post("/post", authValidation, schemaValidation(postSchema), postPosts)
-postsRouter.get("/get-posts/:date", getPosts)
-postsRouter.get("/get-posts-login/:date", getPostsLogin)
+postsRouter.get("/get-posts/:page", getPosts)
+postsRouter.get("/get-posts-login/:page", authValidation, getPostsLogin)
 postsRouter.delete("/delete-post/:user_id/:id", authValidation,  validateDeletePost, deletePost)
 postsRouter.post("/like-post/:id", authValidation, likePost);
 
